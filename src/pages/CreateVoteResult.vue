f6b3bfbba35999787660c7ae3200fd1a3ca670b7
shawon707 <shawon@erp2all.com>
2022-09-09 02:46:16 +0600

    Vote Result Nested Loop Done

--- src/pages/CreateVoteResult.vue
+++ src/pages/CreateVoteResult.vue
@@ -15,22 +15,24 @@
                 <div class="border p-3 rounded">
                   <h3 class="mb-0 text-uppercase">ভোটের ফলাফল তৈরী করুন</h3>
                   <hr />
-                  <form class="row g-3" @submit.prevent="storeCenter">
+                  <form class="row g-3" @submit.prevent="storeVote">
                     <div class="col-12">
                       <label class="form-label">বিভাগ</label>
                       <div class="input-group">
-                        <button class="btn btn-outline-secondary" type="button"><i class='bx bx-search'></i>
+                        <button class="btn btn-outline-secondary" type="button">
+                          <i class="bx bx-search"></i>
                         </button>
                         <select
-                            class="form-select single-select"
-                            id="inputGroupSelect06"
-                            v-model="form.division"
+                          class="form-select single-select"
+                          id="division"
+                          @change="clickDiv($event)"
+                          v-model="form.division"
                         >
                           <option selected>বাছাই করুন</option>
                           <option
-                              :value="division.id"
-                              v-for="division in divisions"
-                              :key="division.id"
+                            :value="division.id"
+                            v-for="division in divisions"
+                            :key="division.id"
                           >
                             {{ division.division_name }}
                           </option>
@@ -40,18 +42,20 @@
                     <div class="col-12">
                       <label class="form-label">জেলা</label>
                       <div class="input-group">
-                        <button class="btn btn-outline-secondary" type="button"><i class='bx bx-search'></i>
+                        <button class="btn btn-outline-secondary" type="button">
+                          <i class="bx bx-search"></i>
                         </button>
                         <select
-                            class="form-select single-select"
-                            id="inputGroupSelect02"
-                            v-model="form.district"
+                          class="form-select single-select"
+                          id="district"
+                          @change="clickDis($event)"
+                          v-model="form.district"
                         >
-                          <option selected>বাছাই করুন</option>
+                          <option value="0">বাছাই করুন</option>
                           <option
-                              :value="district.id"
-                              v-for="district in constituencies"
-                              :key="district.id"
+                            :value="district.id"
+                            v-for="district in districts"
+                            :key="district.id"
                           >
                             {{ district.district_name }}
                           </option>
@@ -61,18 +65,20 @@
                     <div class="col-12">
                       <label class="form-label">আসন</label>
                       <div class="input-group">
-                        <button class="btn btn-outline-secondary" type="button"><i class='bx bx-search'></i>
+                        <button class="btn btn-outline-secondary" type="button">
+                          <i class="bx bx-search"></i>
                         </button>
                         <select
-                            class="form-select single-select"
-                            id="inputGroupSelect05"
-                            v-model="form.constituency"
+                          class="form-select single-select"
+                          id="constituency"
+                          @change="clickCons($event)"
+                          v-model="form.constituency"
                         >
                           <option selected>বাছাই করুন</option>
                           <option
-                              :value="constituency.id"
-                              v-for="constituency in constituencies"
-                              :key="constituency.id"
+                            :value="constituency.id"
+                            v-for="constituency in constituencies"
+                            :key="constituency.id"
                           >
                             {{ constituency.seat }}
                           </option>
@@ -83,20 +89,21 @@
                     <div class="col-12">
                       <label class="form-label">কেন্দ্র</label>
                       <div class="input-group">
-                        <button class="btn btn-outline-secondary" type="button"><i class='bx bx-search'></i>
+                        <button class="btn btn-outline-secondary" type="button">
+                          <i class="bx bx-search"></i>
                         </button>
                         <select
-                            class="form-select single-select"
-                            id="inputGroupSelect04"
-                            v-model="form.center"
+                          class="form-select single-select"
+                          id="inputGroupSelect04"
+                          v-model="form.center"
                         >
                           <option selected>বাছাই করুন</option>
                           <option
-                              :value="center.id"
-                              v-for="center in centers"
-                              :key="center.id"
+                            :value="center.id"
+                            v-for="center in centers"
+                            :key="center.id"
                           >
-                            {{center.center }}
+                            {{ center.center }}
                           </option>
                         </select>
                       </div>
@@ -104,42 +111,104 @@
                     <div class="col-12">
                       <label class="form-label">মোট বৈধ ভোট </label>
                       <input
-                          type="text"
-                          class="form-control"
-                          id="code"
-                          placeholder="মোট ভোট"
-                          v-model="form.valid_vote"
+                        type="text"
+                        class="form-control"
+                        id="code"
+                        placeholder="মোট ভোট"
+                        v-model="form.valid_vote"
                       />
                     </div>
                     <div class="col-12">
                       <label class="form-label">মোট বাতিল ভোট</label>
                       <input
-                          type="text"
-                          class="form-control"
-                          id="code"
-                          placeholder="সেন্টারের কোড"
-                          v-model="form.disqualify_vote"
+                        type="text"
+                        class="form-control"
+                        id="code"
+                        placeholder="সেন্টারের কোড"
+                        v-model="form.disqualify_vote"
                       />
                     </div>
                     <div class="col-12">
                       <label class="form-label">প্রদত্ত ভোট</label>
                       <input
-                          type="text"
-                          class="form-control"
-                          id="code"
-                          placeholder="সেন্টারের কোড"
-                          v-model="form.casted_vote"
+                        type="text"
+                        class="form-control"
+                        id="code"
+                        placeholder="সেন্টারের কোড"
+                        v-model="form.casted_vote"
                       />
                     </div>
                     <div class="col-12">
                       <label class="form-label">শতকরা ভোট</label>
                       <input
-                          type="text"
-                          class="form-control"
-                          id="code"
-                          placeholder="সেন্টারের কোড"
-                          v-model="form.vote_percentage"
+                        type="text"
+                        class="form-control"
+                        id="code"
+                        placeholder="সেন্টারের কোড"
+                        v-model="form.vote_percentage"
                       />
+                    </div>
+                    <div class="col-md-">
+                      <br /><br />
+                      <a class="btn btn-success" @click="addCandidate">
+                        প্রার্থী যোগ করুন
+                      </a>
+                      <br />
+                    </div>
+                    <div class="col-md-12">
+                      <div
+                        class="previous"
+                        v-for="(candidate, counter) in form.candidates"
+                        v-bind:key="counter"
+                      >
+                        <div class="border p-3 rounded">
+                          <div class="col-12">
+                            <label class="form-label" for="duration"
+                              >প্রার্থী</label
+                            >
+                            <div class="input-group">
+                              <button
+                                class="btn btn-outline-secondary"
+                                type="button"
+                              >
+                                <i class="bx bx-search"></i>
+                              </button>
+                              <select
+                                class="form-select single-select"
+                                id='"dd"{{counter++}}'
+                                v-model="form.candidates[counter].candidate"
+                              >
+                                <option selected>বাছাই করুন</option>
+                                <option
+                                  :value="politicalCandidate.id"
+                                  v-for="politicalCandidate in politicalCandidates"
+                                  :key="politicalCandidate.id"
+                                >
+                                  {{ politicalCandidate.candidate_name }}
+                                </option>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-12">
+                            <label class="form-label">ভোট</label>
+                            <input
+                              type="text"
+                              class="form-control"
+                              id="code"
+                              placeholder="ভোট"
+                              v-model="form.candidates[counter].vote"
+                            />
+                          </div>
+                          <div class="col-md-6 mt-3">
+                            <a
+                              class="btn btn-danger"
+                              @click="deleteCandidate(counter)"
+                            >
+                              প্রার্থী মুছে ফেলুন
+                            </a>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                     <div class="col-12">
                       <div class="d-grid">
@@ -180,12 +249,27 @@ export default {
   },
   data() {
     return {
+      votings: [],
+      divisions: [],
+      districts: [],
       centers: [],
       constituencies: [],
+      politicalCandidates: [],
       form: {
+        division: null,
+        district: null,
+        constituency: null,
         center: null,
-        constituency: 0,
-        code: 0,
+        valid_vote: 0,
+        disqualify_vote: 0,
+        casted_vote: 0,
+        vote_percentage: 0,
+        candidates: [
+          {
+            candidate: null,
+            vote: 0,
+          },
+        ],
       },
       errors: {},
     };
@@ -199,71 +283,106 @@ export default {
     SidebarPart,
   },
   methods: {
-    async respond(status, id) {
-      let users = JSON.parse(localStorage.getItem("users"));
-      axios.defaults.headers.common["Authorization"] = "JWT " + users.token;
-      return axios
-          .patch(
-              process.env.VUE_APP_BASE_URL +
-              "/api/v1/pera/region-admin/center/" +
-              id +
-              "/",
-              { status: status }
-          )
-          .then((response) => {
-            if (response) {
-              this.allConstituency();
-              this.allCenters();
-            }
-            return true;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
+    addCandidate() {
+      this.form.candidates.push({
+        candidate: null,
+        vote: 0,
+      });
     },
-    async allConstituency() {
+    deleteCandidate(counter) {
+      this.form.candidates.splice(counter, 1);
+    },
+    clickDiv(event) {
+      this.constituencies = [];
+      this.centers = [];
+      let division = event.target.value;
+      this.allDistricts(division);
+    },
+    clickDis(event) {
+      this.centers = [];
+      let district = event.target.value;
+      this.allConstituency(district);
+    },
+    clickCons(event) {
+      let constituency = event.target.value;
+      this.allCenters(constituency);
+    },
+    async allDivisions() {
+      await axios
+        .get(process.env.VUE_APP_BASE_URL + "/api/v1/pera/public/division/")
+        .then(({ data }) => (this.divisions = data.data))
+        .catch();
+    },
+    async allDistricts(division) {
+      await axios
+        .get(
+          process.env.VUE_APP_BASE_URL +
+            "/api/v1/pera/public/district/?division=" +
+            division
+        )
+        .then(({ data }) => (this.districts = data.data))
+        .catch();
+    },
+    async allConstituency(district) {
+      await axios
+        .get(
+          process.env.VUE_APP_BASE_URL +
+            "/api/v1/pera/public/constituency/?district=" +
+            district
+        )
+        .then(({ data }) => (this.constituencies = data.data))
+        .catch();
+    },
+    async allCenters(constituency) {
+      await axios
+        .get(
+          process.env.VUE_APP_BASE_URL +
+            "/api/v1/pera/public/center/?constituency=" +
+            constituency
+        )
+        .then(({ data }) => (this.centers = data.data))
+        .catch();
+    },
+    async allVotingInfos() {
       let users = JSON.parse(localStorage.getItem("users"));
       axios.defaults.headers.common["Authorization"] = "JWT " + users.token;
       await axios
-          .get(
-              process.env.VUE_APP_BASE_URL +
-              "/api/v1/pera/region-admin/constituency/"
-          )
-          .then(({ data }) => (this.constituencies = data.data.results))
-          .catch();
+        .get(process.env.VUE_APP_BASE_URL + "/api/v1/pera/result/voting/")
+        .then(({ data }) => (this.votings = data.data.results))
+        .catch();
     },
-    async allCenters() {
+    async allPoliticalCandidates() {
       let users = JSON.parse(localStorage.getItem("users"));
       axios.defaults.headers.common["Authorization"] = "JWT " + users.token;
       await axios
-          .get(process.env.VUE_APP_BASE_URL + "/api/v1/pera/region-admin/center/")
-          .then(({ data }) => (this.centers = data.data.results))
-          .catch();
+        // .get(process.env.VUE_APP_BASE_URL + "/api/v1/pera/public/political-candidate/")
+        .get(process.env.VUE_APP_BASE_URL + "/api/v1/pera/political/candidate/")
+        .then(({ data }) => (this.politicalCandidates = data.data.results))
+        .catch();
     },
-    async storeCenter() {
+    async storeVote() {
       let users = JSON.parse(localStorage.getItem("users"));
       axios.defaults.headers.common["Authorization"] = "JWT " + users.token;
       return axios
-          .post(
-              process.env.VUE_APP_BASE_URL + "/api/v1/pera/region-admin/center/",
-              this.form
-          )
-          .then((response) => {
-            if (response) {
-              this.allConstituency();
-              this.allCenters();
-              alert(response)
-            }
-            return true;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
+        .post(
+          process.env.VUE_APP_BASE_URL + "/api/v1/pera/result/voting/",
+          this.form
+        )
+        .then((response) => {
+          if (response) {
+            this.$router.push({ name: "District" });
+          }
+          return true;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
   },
   created() {
-    this.allConstituency();
-    this.allCenters();
+    console.log(this.allVotingInfos());
+    this.allDivisions();
+    this.allPoliticalCandidates();
   },
 };
 </script>
